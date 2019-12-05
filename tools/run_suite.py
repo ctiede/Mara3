26e7072b546af1e1ab32de21734b00af44a0a66e
Chris <cwt271@nyu.edu>
2019-12-05 11:40:52 -0500

    Can now launch suites to test parallel scaling -- need to test

--- tools/run_suite.py
+++ tools/run_suite.py
@@ -24,12 +24,15 @@ Date: {date}
 def mara_command(subprog, exe='mara', **kwargs):
     return './{} {} {}'.format(exe, subprog,
         ' '.join(['{}={}'.format(k, v) for k, v in kwargs.items()]))
+    # return './{} {} {}'.format('mpiexe -np 2 mara', subprog,
+    # ' '.join(['{}={}'.format(k, v) for k, v in kwargs.items()]))
 
 
 
-def run_script(template, subprog, exe='mara', runid='test', nodes=1, hours=8, **kwargs):
+def run_script(template, subprog, exe='mara', runid='test', nodes=1, ppn=1, hours=8, **kwargs):
     return template.format(
         nodes=nodes,
+        ppn=ppn,
         hours=hours,
         job_name=runid,
         output=os.path.join(kwargs.get('outdir', './'), runid + '.out'),
@@ -62,7 +65,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('suite_file', help="Python file defining a 'suite' variable")
     parser.add_argument('--machine-file', default='machine.py', help="Python file defining a 'machine' variable")
-    parser.add_argument('--submit', action='store_true', help="Also launch jobs")
+    parser.add_argument('--submit', '-s', action='store_true', help="Also launch jobs")
     parser.add_argument('--quiet', '-q', action='store_true', help="Suppress printing of submit script contents to the terminal")
     args = parser.parse_args()
 
@@ -77,16 +80,23 @@ if __name__ == "__main__":
         run_dir             = os.path.join(str(suite_dir), suite_name, runid)
         submit_file         = os.path.join(run_dir, 'submit.sh')
         readme_file         = os.path.join(run_dir, 'README')
+        job_params          = suite['job_params']
         mara_opts['outdir'] = run_dir
-        mara_opts.update(suite['runs'][runid])
+
+        if suite['scaling_flag']: job_params.update(suite['runs'][runid])
+        else: mara_opts.update(suite['runs'][runid])
 
         submit_content = run_script(
             machine['submit_script'],
             suite['subprog'],
             exe=suite.get('exe', 'mara'),
             runid=runid,
-            nodes=suite['job_params']['nodes'],
-            hours=suite['job_params']['hours'],
+            # nodes=suite['job_params']['nodes'],
+            # ppn  =suite['job_params']['ppn'],
+            # hours=suite['job_params']['hours'],
+            nodes=job_params['nodes'],
+            ppn  =job_params['ppn'],
+            hours=job_params['hours'],
             **mara_opts)
 
         readme_content = readme_template.format(
